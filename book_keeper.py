7331663901469a8ef1b9ea38cb65b4a92b2769ca
Istvan Gellai <istvan.gellai@sagemcom.com>
2022-10-02 19:59:03 +0200

    Structural refactor

--- book_keeper.py
+++ book_keeper.py
@@ -3,56 +3,108 @@ import subprocess
 from collect_names import *
 from media_utils import *
 
-tempath = ''
-music_path=''
-video_path=''
-output_path=''
+from preprocess import *
 
-input_path=''
-dictionary_path=''
-chapter_delimiter='<<LIMIT>>'
+class BookKeeper:
+    tempath = ''
+    music_path=''
+    video_path=''
+    output_path=''
 
-collect_names=False
-image_path=''
-background_music='./deep2.mp3'
-music_weight='3 0.82'
+    input_path=''
+    dictionary_path=''
+    chapter_delimiter='<<LIMIT>>'
+
+    collect_names=False
+    image_path=''
+    background_music='./deep2.mp3'
+    music_weight='3 0.82'
+    sumlen=0
+
+    def GenerateMP4 (self, name):
+        command="./ffmpeg.exe -stats -i " + self.music_path + name + ".mp3 -f null -"
+
+        with open(self.tempath + "stdout.txt","wb") as out, open(self.tempath + "stderr.txt","wb") as err:
+            subprocess.call(command,stdout=out,stderr=err)
+        seconds=0
+
+        with open(self.tempath + "stderr.txt","r") as file1:
+            Lines = file1.readlines()
+            for line in Lines:
+                if ("Duration:" in line):
+                    times=line.split()[1].split('.')[0].split(":")
+                    seconds=int(times[0])*3600 + int(times[1])*60+int(times[2])
+                    print(line)
+                    print(seconds)
+
+        rate = '1' if seconds < 150 else '0.1'
+        command="./ffmpeg.exe -loop 1 -framerate 1 -i " + self.music_path + "cover.png -i " + self.music_path + name + ".mp3 -i " + self.background_music + ".mp3 -ss 0 -t " + str(seconds) + " -filter_complex amix=inputs=2:duration=longest:weights=\"" + self.music_weight + "\" -c:v libx264 -r " + rate + " -movflags +faststart " + self.video_path + name + ".mp4"
+        subprocess.call(command)
+        file_object = open(self.output_path+"/list.txt", 'a', encoding='utf-8')
+        file_object.write("file 'mp4/" + name + ".mp4'\n")
+        print(name + 'video')
+
+    def CheckPartLength(self, name, sumlength) :
+        hour = int(sumlength/3600)
+        minute = int((sumlength - (hour*3600))/60)
+        seconds = int(sumlength - (hour * 3600) - (minute * 60))
+        AppendtoFile(self.output_path + "/lenghts.txt", str(hour) + ":" + format(minute, "02d") + ":" + format(seconds, "02d"))
+
+        seconds = GetMediaLen(self.video_path + name + ".mp4")
+        print(seconds)
+        return self.sumlen + seconds
+
+    def Execute(self):
+        # Generate media
+        speaker = init_speaker()
+        # exit(0)
+
+        if os.path.isdir(self.output_path):
+            shutil.rmtree(self.output_path)
+        os.makedirs(self.output_path, 0o777)
+        if not self.collect_names:
+            os.makedirs(self.music_path)
+        if self.image_path:
+            self.video_path = self.output_path + "/mp4/"
+            os.makedirs(self.video_path)
+        os.makedirs(self.tempath)
+
+        with open(self.input_path, "r", encoding='utf-8') as input:
+            input_file = preprocess(input.read())
+
+            if self.dictionary_path != '' :
+                with open(self.dictionary_path, "r", encoding='utf-8') as dict_file:
+                    pronunciation = dict_file.readlines()
+
+                    for line in pronunciation:
+                        values = line.split('\t')
+                        input_file=input_file.replace(values[0].lstrip(), values[1].rstrip())
+
+            if self.collect_names :
+                CollectNames(input_file, self.output_path)
+                exit(0)
+
+            sections=input_file.split(self.chapter_delimiter)
+
+            index=0
+            for section in sections:
+                name = format(int(index), "02d")
+                speaker.save_to_file(section, self.music_path + name + '.mp3')
+                speaker.runAndWait()
+                if self.image_path:
+                    shutil.copyfile(self.image_path, self.music_path + "cover.png")
+                    self.GenerateMP4(name)
+                    self.sumlen = self.CheckPartLength(name, self.sumlen)
+                index += 1
+
+        command="./ffmpeg.exe -f concat -i " + self.output_path + "/list.txt -c copy " + self.output_path + "/output.mp4"
+        subprocess.call(command)
+        shutil.move(self.output_path + "/list.txt", self.tempath + "list.txt")
+        shutil.rmtree(self.tempath)
 
 
-def GenerateMP4 (name):
-    command="./ffmpeg.exe -stats -i " + music_path + name + ".mp3 -f null -"
 
-    with open(tempath + "stdout.txt","wb") as out, open(tempath + "stderr.txt","wb") as err:
-        subprocess.call(command,stdout=out,stderr=err)
-    seconds=0
-
-    with open(tempath + "stderr.txt","r") as file1:
-        Lines = file1.readlines()
-        for line in Lines:
-            if ("Duration:" in line):
-                times=line.split()[1].split('.')[0].split(":")
-                seconds=int(times[0])*3600 + int(times[1])*60+int(times[2])
-                print(line)
-                print(seconds)
-
-    rate = '1' if seconds < 150 else '0.1'
-    command="./ffmpeg.exe -loop 1 -framerate 1 -i " + music_path + "cover.png -i " + music_path + name + ".mp3 -i " + background_music + ".mp3 -ss 0 -t " + str(seconds) + " -filter_complex amix=inputs=2:duration=longest:weights=\"" + music_weight + "\" -c:v libx264 -r " + rate + " -movflags +faststart " + video_path + name + ".mp4"
-    subprocess.call(command)
-    file_object = open(output_path+"/list.txt", 'a', encoding='utf-8')
-    file_object.write("file 'mp4/" + name + ".mp4'\n")
-    print(name + 'video')
-
-
-def CheckPartLength(name, sumlength) :
-    hour = int(sumlength/3600)
-    minute = int((sumlength - (hour*3600))/60)
-    seconds = int(sumlength - (hour * 3600) - (minute * 60))
-    AppendtoFile(output_path + "/lenghts.txt", str(hour) + ":" + format(minute, "02d") + ":" + format(seconds, "02d"))
-
-    seconds = GetMediaLen(video_path + name + ".mp4")
-    print(seconds)
-    return sumlen + seconds
-
-
+object = BookKeeper()
 try:
     opts, args = getopt.getopt(sys.argv[1:], "ho:i:fr:l:p:b:w:", ["help", "output="])
 except getopt.GetoptError as err:
@@ -65,81 +117,32 @@ for o, a in opts:
         # usage()
         sys.exit()
     elif o in ("-o", "--output"):
-        output_path = a
+        object.output_path = a
     elif o in ("-i", "--input"):
-        input_path = a
+        object.input_path = a
     elif o in ("-f", "--find-names"):
-        collect_names=True
+        object.collect_names=True
     elif o in ("-r", "--replace"):
-        dictionary_path = a
+        object.dictionary_path = a
     elif o in ("-l", "--limit"):
-        chapter_delimiter = a
+        object.chapter_delimiter = a
     elif o in ("-w", "--weight"):
-        music_weight = a
+        object.music_weight = a
     elif o in ("-b", "--bacground"):
-        background_music = a
+        object.background_music = a
     elif o in ("-p", "--picture"):
-        image_path = a
+        object.image_path = a
     else:
         assert False, "unhandled option"
 # ...
 
-if not input_path:
+if not object.input_path:
     exit (1)
 
-if output_path == '':
+if object.output_path == '':
     output_path='output'
 
-music_path = output_path+"/mp3/"
-tempath=output_path + "/tmp/"
+object.music_path = object.output_path+"/mp3/"
+object.tempath=object.output_path + "/tmp/"
 
-
-# Generate media
-speaker = init_speaker()
-# exit(0)
-
-
-if os.path.isdir(output_path):
-    shutil.rmtree(output_path)
-os.makedirs(output_path, 0o777)
-if not collect_names:
-    os.makedirs(music_path)
-if image_path:
-    video_path = output_path + "/mp4/"
-    os.makedirs(video_path)
-os.makedirs(tempath)
-
-
-with open(input_path, "r", encoding='utf-8') as input:
-    input_file = input.read()
-
-    if dictionary_path != '' :
-        with open(dictionary_path, "r", encoding='utf-8') as dict_file:
-            pronunciation = dict_file.readlines()
-
-            for line in pronunciation:
-                values = line.split('\t')
-                input_file=input_file.replace(values[0].lstrip(), values[1].rstrip())
-
-    if collect_names :
-        CollectNames(input_file, output_path)
-        exit(0)
-
-    sections=input_file.split(chapter_delimiter)
-
-    index=0
-    sumlen=0
-    for section in sections:
-        name = format(int(index), "02d")
-        speaker.save_to_file(section, music_path + name + '.mp3')
-        speaker.runAndWait()
-        if image_path:
-            shutil.copyfile(image_path, music_path + "cover.png")
-            GenerateMP4(name)
-            sumlen = CheckPartLength(name, sumlen)
-        index += 1
-
-command="./ffmpeg.exe -f concat -i " + output_path + "/list.txt -c copy " + output_path + "/output.mp4"
-subprocess.call(command)
-shutil.move(output_path + "/list.txt", tempath + "list.txt")
-shutil.rmtree(tempath)
+object.Execute()
